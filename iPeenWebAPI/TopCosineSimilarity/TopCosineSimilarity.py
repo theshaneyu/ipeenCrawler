27d434135a4adc7f81dc72be31c0e01c0aa96696
theshaneyu <shaneyu07@gmail.com>
2017-04-03 08:19:18 +0000

    TopCosineSimilarity Changed to Top n result, templates added in django project

--- iPeenWebAPI/TopCosineSimilarity/TopCosineSimilarity.py
+++ iPeenWebAPI/TopCosineSimilarity/TopCosineSimilarity.py
@@ -1,8 +1,13 @@
 import numpy as np
 from gensim import models
+import operator
+from bson.objectid import ObjectId
 
 
 class GetTopResult(object):
+    """
+    
+    """
     def __init__(self, modelPath, uri):
         from pymongo import MongoClient
         self.client = MongoClient(uri)
@@ -11,7 +16,7 @@ class GetTopResult(object):
         self.modelPath = modelPath
 
 
-    def getArticle(self, inputTermList):
+    def getArticle(self, inputTermList, topNum):
         model = models.KeyedVectors.load_word2vec_format(self.modelPath, binary=True)
         uninitialized = True
         for item in inputTermList:
@@ -24,27 +29,34 @@ class GetTopResult(object):
             except:
                 continue
         # 至此已經得到使用者Query的所有Term的加總向量，為sumVec
-        # self.getMostSimilar(sumVec)
-        ID = self.getMostSimilar(sumVec)
-        resultArticle = self.coll.find({'ID':ID})
-        # print(type(resultArticle[0])) # <class 'dict'>
-        return resultArticle[0]
+
+        # self.getMostSimilar(sumVec, topNum)
+        idList = self.getMostSimilar(sumVec, topNum)
+        resultList = list()
+        for item in idList:
+            resultArticle = self.coll.find({"_id": ObjectId(item)}, {'Vector': False, 'ID': False})
+            # print(type(resultArticle[0])) # <class 'dict'>
+            resultList.append(resultArticle[0])
+        
+        return resultList
 
 
-
-
-    def getMostSimilar(self, queryVec):
-        allVectors = self.coll.find({}, {'Vector':True, 'ID':True, '_id':False})
-        topCosineSimilarity = 0.0
+    def getMostSimilar(self, queryVec, num):
+        allVectors = self.coll.find({}, {'Vector':True, '_id':True})
+        # topCosineSimilarity = 0.0
+        vecDict = dict()
         for vector in allVectors:
             array = np.array(vector['Vector']) # 資料庫裡的Vector欄位，當初是以.tolist()存進json，則拿出來要這樣復原
             cosineSimilarity = np.dot(queryVec, array)/(np.linalg.norm(queryVec) * np.linalg.norm(array))
-        # print(topCosineSimilarity)
-            if cosineSimilarity > topCosineSimilarity:
-                topCosineSimilarity = cosineSimilarity
-                topResultID = vector['ID']
-        # print(type(topResultID))
-        return topResultID
+            vecDict[vector['_id']] = cosineSimilarity
+        
+        sorted_vecDict = sorted(vecDict.items(), key=operator.itemgetter(1), reverse=True)
+        objectIDList = list()
+        for item in sorted_vecDict[0:num]:
+            objectIDList.append(item[0])
+
+        return objectIDList # 回傳[58d8e3850383b11732f87a0d, 58d8e3860383b11732f87c76]
+
 
 
     def testMongo(self):
@@ -67,7 +79,8 @@ if __name__ == '__main__':
         queryList.append(item)
 
     obj = GetTopResult('./med250.model.bin', 'mongodb://140.120.13.244:7777/')
-    print(obj.getArticle(queryList)['Content'])
-    # obj.main()
+    # for item in obj.getArticle(queryList, 10):
+    #     print(item)
+    print(obj.getArticle(queryList, 2))
 
 
--- iPeenWebAPI/getArticles/views.py
+++ iPeenWebAPI/getArticles/views.py
@@ -6,19 +6,18 @@ from TopCosineSimilarity.TopCosineSimilarity import GetTopResult
 def get(request):
     obj = GetTopResult('./TopCosineSimilarity/med250.model.bin', 'mongodb://140.120.13.244:7777/')
     termStr = request.GET['term']
+    num = request.GET['num']
     termList = termStr.split()
     print(termList)
-    returnDict = obj.getArticle(termList)
-    Top20Str = ''
-    for item in returnDict['Top20']:
-        Top20Str += (item + ' / ')
+    returnList = obj.getArticle(termList, int(num)) # 回傳[文章資訊dict, ... , 文章資訊dict]
+    
+    showList = list()
+    for item in returnList:
+        innerList = list()
+        innerList.append(item['Content'])
+        innerList.append(item['Top20'])
+        showList.append(innerList)
 
-    # returnStr = returnDict['Content'] + '{Top20 keywords : [' + Top20Str + ']}'
-    # print(type(termStr)) # <class 'str'>
-    # return JsonResponse(returnDict, safe = False)
-    # return HttpResponse('%s' % returnStr)
-    # return HttpResponse('%s' % returnDict['Content'])
-    return HttpResponse('<p><strong> 文章內容 </strong></p>'
-                        '<p>' + returnDict['Content'] + '</p>'
-                        '<p><strong> 前20關鍵字 </strong></p>'
-                        '<p>' + Top20Str + '</p>')
+    # print(showList)
+    context = {'showList': showList}
+    return render(request, 'getArticles/index.html', context)
